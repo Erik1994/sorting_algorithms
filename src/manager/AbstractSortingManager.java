cd4f74d5942e5c774bb3700eb6fcb74a6b90389f
erikantonyan <erik.antonyan@picsart.com>
2022-08-26 12:41:21 +0400

    Add setter for algorithm in AbstractSortingManager

--- src/manager/AbstractSortingManager.java
+++ src/manager/AbstractSortingManager.java
@@ -5,4 +5,7 @@ import algorithm.Algorithm;
 public abstract class AbstractSortingManager<T extends Integer> {
     protected Algorithm<T> algorithm;
     public abstract void sort(T[] array);
+    public void setAlgorithm(Algorithm<T> algorithm) {
+        this.algorithm = algorithm;
+    }
 }
