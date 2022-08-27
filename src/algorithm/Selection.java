99faedf5b84889f44cc53b8cd73f38bc57423f89
Erik Antonyan <erik.antonyan1994@gmail.com>
2022-08-27 17:11:57 +0400

    Minor change

--- src/algorithm/Selection.java
+++ src/algorithm/Selection.java
@@ -13,11 +13,9 @@ public class Selection<T extends Integer> extends Algorithm<T> {
     @Override
     public void sort(T[] array) {
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; --lastUnsortedIndex) {
-            T max = array[0];
             int maxIndex = 0;
             for (int i = 1; i <= lastUnsortedIndex; ++i) {
-                if (array[i].compareTo(max) > 0) {
-                    max = array[i];
+                if (array[i].compareTo(array[lastUnsortedIndex]) > 0) {
                     maxIndex = i;
                 }
             }
